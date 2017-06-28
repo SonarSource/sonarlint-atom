0beb8de0cffded632df9def443da845ebc9bcca5
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-06-28 17:45:39 +0200

    Update to work with latest version of language server

--- sonarlint/lib/sonarlint.js
+++ sonarlint/lib/sonarlint.js
@@ -15,7 +15,8 @@ class SonarLintLanguageServer extends AutoLanguageClient {
 
   startServerProcess () {
     // TODO download if not present, see https://github.com/atom/languageserver-java/blob/master/lib/main.js
-    const serverHome = path.join(__dirname, '..', 'server')
+    const basedir = path.join(__dirname, "..")
+    const serverHome = path.resolve(basedir, "server")
     const command = 'java.sh'
     const args = ['-jar', 'sonarlint-ls.jar']
     let process
@@ -28,6 +29,9 @@ class SonarLintLanguageServer extends AutoLanguageClient {
       })
       server.listen(0, '127.0.0.1', () => {
         args.push(server.address().port)
+        args.push(toUrl(path.resolve(basedir, "analyzers", "sonarjs.jar")))
+        args.push(toUrl(path.resolve(basedir, "analyzers", "sonarphp.jar")))
+        args.push(toUrl(path.resolve(basedir, "analyzers", "sonarpython.jar")))
         process = this.spawnServer(command, args, serverHome)
       })
     })
@@ -89,6 +93,17 @@ class CustomLinterPushV2Adapter extends LinterPushV2Adapter {
       }
     ]
   }
+}
+
+function toUrl(filePath) {
+    var pathName = path.resolve(filePath).replace(/\\/g, '/')
+
+    // Windows drive letter must be prefixed with a slash
+    if (pathName[0] !== '/') {
+        pathName = '/' + pathName
+    }
+
+    return encodeURI('file://' + pathName)
 }
 
 module.exports = new SonarLintLanguageServer()
