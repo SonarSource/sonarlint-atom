155506255f45e3969ee4c6122e76401a9bcc873b
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-06-28 17:57:00 +0200

    Remove unused files

--- sonarlint/spec/sonarlint-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SonarlintView from '../lib/sonarlint-view';
-
-describe('SonarlintView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
